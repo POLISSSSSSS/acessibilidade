7c5d731f0ebc116d8feb569765945a11a352734e
Apolo <apolo.branco@escola.pr.gov.br>
2025-03-11 16:05:35 -0300

    11/03

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded',function(){
 const aumentaFonteBotao=document.getElementById('aumentar-fonte');
 let TamanhoAtualFonte=1;
-aumentaFonteBotao.addEventListener('click',function(){})
+aumentaFonteBotao.addEventListener('click',function(){
+    tamanhoAtual +=0.1;
+    document.body.style.fontSize=`${TamanhoAtualFonte}rem`
 
-}
+
+})
+
+
+})
