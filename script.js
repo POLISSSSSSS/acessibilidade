9128a3475eda676d188e43a587edc362ea1e6e01
Apolo <apolo.branco@escola.pr.gov.br>
2025-03-25 13:58:56 -0300

    25/03

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-document.addEventListener('DOMContentLoaded',function(){
-const aumentaFonteBotao=document.getElementById('aumentar-fonte');
-let TamanhoAtualFonte=1;
-aumentaFonteBotao.addEventListener('click',function(){
-    tamanhoAtual +=0.1;
-    document.body.style.fontSize=`${TamanhoAtualFonte}rem`
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-})
-diminuiFonteBotao.addEventListener('click',function(){
-    tamanhoAtual -=0.1;
-    document.body.style.fontSize=`${TamanhoAtualFonte}rem`
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
-})
+    })
 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
 })
